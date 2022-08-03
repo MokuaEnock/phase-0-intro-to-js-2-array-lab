041e367fa25ece6d6ceb87d875be01c781eb227c
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-08-03 22:38:50 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function  destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function  destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    let newCats = cats.slice()
+    newCats.push(name)
+    return newCats;
+}
+
+function prependCat(name){
+    let xCats = cats.slice()
+    xCats.unshift(name)
+    return xCats;
+}
+
+function removeLastCat(){
+    let x = cats.slice()
+    x.pop()
+    return x;
+}
+
+function removeFirstCat(){
+    let y = cats.slice()
+    y.shift()
+    return y
+}
